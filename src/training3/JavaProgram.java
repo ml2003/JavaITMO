bafc8e7c67c3f0327ff4da89e9d4c70d5228b722
@a.karmanova <ml2003@list.ru>
2022-06-23 20:56:55 +0300

    lesson 3-1

---
+++ src/training3/JavaProgram.java
@@ -0,0 +1,11 @@
+package training3;
+
+public class JavaProgram {
+    public static void main (String[] args){
+
+        Study study = new Study ("Изучение Java - это просто!");
+
+        System.out.println(study.printCourse());
+
+    }
+}
---
+++ src/training3/Study.java
@@ -0,0 +1,14 @@
+package training3;
+
+public class Study {
+
+    private  String course;
+
+    public Study(String course) {
+        this.course = course;
+    }
+
+    public String printCourse(){
+        return this.course;
+    }
+}
