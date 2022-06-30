cb9c817c957d7c4f617bb04e702316e60dcb9168
a.karmanova <ml2003@list.ru>
2022-06-30 11:35:55 +0300

    practice 5

---
+++ src/training5/InvertWord.java
@@ -0,0 +1,15 @@
+package training5;
+
+public class InvertWord {
+    public static void main(String[] args) {
+        String str = "Это строка в которой надо инвертировать каждое слово";
+
+        String[] words = new StringBuilder(str).reverse().toString().split(" ");
+
+        StringBuilder reverseWord = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) reverseWord.append(words[i]).append(" ");
+        System.out.println(reverseWord);
+
+    }
+}
